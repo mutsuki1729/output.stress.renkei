86f79f19d46d339c5326d9641d609817bb472294
EC2 Default User <muratsuji.kai@gmail.com>
2023-10-05 13:45:45 +0900

    投稿一覧の表示

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [PostController::class, 'index']);
+
 Route::group(['middleware'=>'auth'],function(){
     Route::group(['prefix'=>'posts/{id}'],function(){
        Route::post('like','LikeController@store')->name('likes.like');
